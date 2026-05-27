3326670b672f11749f3aa4ee75fbb5160e119ae9
sinotten <nick_sinotte@yahoo.com>
2026-05-27 14:19:55 -0500

    adding material ui and first component
    Adding material ui and first component to see if it all works.

---
+++ components/Events/EventDashboardSummary.jsx
@@ -0,0 +1,14 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+
+export function EventDashboardSummary(){
+    return(
+        <div>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <span>Testing</span>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
