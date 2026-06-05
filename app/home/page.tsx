e6ec1e893b2874f505cbbe8e1f92b8888de6fff7
sinotten <nick_sinotte@yahoo.com>
2026-06-04 21:23:59 -0500

    adding contact card
    adding contact card and cleaning up some stuff

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,3 @@
-import { Container, Box, TextField } from "@mui/material";
 import { EventDashboardSummary } from "../../components/Events/EventDashboardSummary";
 import EventDashboardHeader from "../../components/Events/EventDashboardHeader";
 
@@ -9,11 +8,11 @@ export default function Page() {
       {/*left side */}
       <div className="w-3/4 h-full p-2">
       {/* left side inner card */}
-        <div className="flex flex-col bg-white rounded-md w-full h-full" >
+        <div className="flex flex-col bg-white rounded-md w-full h-full border-2 border-slate-300" >
           <div className="w-full">
             <EventDashboardHeader />
           </div>
-          <div className="bg-slate-100 w-full h-full p-5">
+          <div className="bg-slate-50 w-full h-full p-5">
             <EventDashboardSummary />
           </div>
         </div>
@@ -22,19 +21,40 @@ export default function Page() {
       <div className="w-1/4 h-full p-2" >
 
         {/*right side card */}
-        <div className="bg-white rounded-md w-full h-full p-3">
-          <div className="flex flex-col bg-white">
-            <div>
-              <div className="text-black mb-3">
-                <span className="text-3xl">People</span>
-              </div>
-              <div className="mb-3">
-                <input type="text" className="w-full rounded-md ring px-2 py-1"/>
-              </div>
+        <div className="flex flex-col bg-white rounded-md w-full h-full border-2 border-slate-300">
+          {/* top search section */}
+          <div className="w-full p-3">
+            <div className="text-black mb-3">
+              <span className="text-2xl font-semibold">Contacts </span>
             </div>
-            <div className="h-full w-full bg-red-800">
-              <div className="ring">
+            <div>
+              <input type="text" className="w-full rounded-xl ring px-2 py-1 bg-slate-50" placeholder="Search Contacts"/>
+            </div>
+          </div>
+          {/*Bottom display of contacts */}
+          <div className="h-full w-full bg-slate-50 p-5">
 
+            {/* contact card */}
+            <div className="border border-slate-400 border-solid w-full h-fit bg-white rounded-sm shadow-md">
+              <div className="flex">
+                {/*profile image */}
+                <div className="py-2 px-4 w-fit h-full">
+                  <div className="w-10 h-10 rounded-full border border-black">
+                    {/*profile image here */}
+                  </div>
+                </div>
+
+                {/*information section*/}
+                <div className="h-full w-full mt-auto mb-auto">
+                  <div className="w-fit h-fit">
+                    <div>
+                      <span>test mctesterson</span>
+                    </div>
+                    <div>
+                      <span>September 27th, 1990</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- components/Events/EventDashboardHeader.jsx
+++ components/Events/EventDashboardHeader.jsx
@@ -3,14 +3,20 @@ import { Box, TextField } from "@mui/material";
 export default function EventDashboardHeader(){
   return(
         <div className="bg-white p-3 rounded-t-md">
-          <div className="mb-3 text-black">
-            <span className="text-3xl">Available Events</span>
+          <div className="flex justify-between items-center">
+            <div className="mb-2 text-black">
+              <span className="text-3xl font-semibold">Available Events</span>
+            </div>
+            <div className="h-full content-center mb-3 text-white">
+              <button className="p-3 rounded-xl bg-blue-700 hover:bg-blue-800 cursor-pointer font-semibold active:bg-blue-900">+ Add Event</button>
+            </div>
           </div>
           <div className="bg-gray-50 w-full h-fit rounded-xl border">
             <input 
               style={{ width: "100%", borderRadius: 10 }} 
               className="py-1 px-3" 
-              label="Search"              
+              label="Search"      
+              placeholder="Search Events..."        
               />
           </div>
         </div>
